4733d61ff35f2216adec4501fecc00e2280786e7
Sunil Gautam <sunil.gautam@kronosis.com>
2016-10-25 17:34:22 +0530

    Refactoring done

--- index.android.js
+++ index.android.js
@@ -1,40 +1,4 @@
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  TextInput,
-  View,
-  TouchableHighlight,
-} from 'react-native';
-
-import { createStore, compose, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
-import createLogger from 'redux-logger';
-import rootReducer from './src/reducers';
+import { AppRegistry } from 'react-native';
 import App from './src/App';
 
-function configureStore(initialState) {
-  // store enhancers
-  var enhancer = compose(
-    applyMiddleware(
-      createLogger()
-    )
-  );
-
-  return createStore(rootReducer, initialState, enhancer);
-}
-
-const store = configureStore({});
-
-export default class ReduxTodo extends Component {
-  render() {
-    return (
-      <Provider store={store}>
-        <App />
-      </Provider>
-    );
-  }
-}
-
-AppRegistry.registerComponent('ReduxTodo', () => ReduxTodo);
+AppRegistry.registerComponent('ReduxTodo', () => App);
--- src/App.js
+++ src/App.js
@@ -1,129 +1,31 @@
 import React, { Component } from 'react';
-import {
-  StyleSheet,
-  Text,
-  TextInput,
-  View,
-  TouchableHighlight,
-} from 'react-native';
-import dismissKeyboard from 'react-native/Libraries/Utilities/dismissKeyboard'
+import { createStore, compose, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import createLogger from 'redux-logger';
+import rootReducer from './reducers';
+import Home from './scenes/Home';
 
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
-import * as ActionCreators from './actions';
+function configureStore(initialState) {
+  // store enhancers
+  var enhancer = compose(
+    applyMiddleware(
+      createLogger()
+    )
+  );
 
-import TodoList from './components/TodoList';
+  return createStore(rootReducer, initialState, enhancer);
+}
+
+const store = configureStore({});
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      todoTitle: '',
-    };
-
-    this.addTodoPressed = this.addTodoPressed.bind(this);
-  }
-
-  addTodoPressed() {
-    // check blank
-    if (!this.state.todoTitle) {
-      return;
-    }
-
-    const todo = {
-      id: Math.random().toString(36),
-      title: this.state.todoTitle,
-    };
-    // call action
-    this.props.actions.addTodo(todo);
-
-    // clear text input
-    this.setState({ todoTitle: '' });
-
-    // dismiss keyboard
-    dismissKeyboard();
-  }
-
-  renderSeparator(sectionID, rowID, adjacentRowHighlighted) {
-    return (
-      <View
-        key={`${sectionID}-${rowID}`}
-        style={styles.separator}
-      />
-    );
-  }
-
   render() {
     return (
-      <View style={[styles.container]}>
-        <View style={styles.inputContainer}>
-          <TextInput
-            value={this.state.todoTitle}
-            onChangeText={(newTodoTitle) => { this.setState({ todoTitle: newTodoTitle }); }}
-            onSubmitEditing={this.addTodoPressed}
-            style={styles.textinput}
-            underlineColorAndroid="transparent"
-            placeholder="Your next todo ..."
-            autoFocus={false}
-          />
-          <TouchableHighlight
-            onPress={this.addTodoPressed}
-            activeOpacity={0.5}
-            style={{ marginLeft: 5,borderRadius: 5, }}
-            underlayColor="gray"
-          >
-            <Text style={styles.button}>Add</Text>
-          </TouchableHighlight>
-        </View>
-        <TodoList todos={this.props.todos} />
-      </View>
+      <Provider store={store}>
+        <Home />
+      </Provider>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#F5FCFF',
-    padding: 5,
-  },
-  inputContainer: {
-    paddingBottom: 5,
-    marginBottom: 5,
-    borderBottomWidth: 1,
-    borderBottomColor: '#eeeeee',
-    flexDirection: 'row',
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  textinput: {
-    borderColor: '#cccccc',
-    backgroundColor: 'white',
-    borderWidth: 1,
-    paddingTop: 5,
-    paddingBottom: 5,
-    paddingLeft: 10,
-    paddingRight: 10,
-    fontSize: 16,
-    flex: 1,
-  },
-  button: {
-    backgroundColor: 'skyblue',
-    borderRadius: 5,
-    padding: 10,
-  },
-});
-
-function mapStateToProps(state) {
-  return {
-    todos: state.todos,
-  };
-}
-
-function mapDispatchToProps(dispatch) {
-  return {
-    actions: bindActionCreators(ActionCreators, dispatch)
-  };
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
